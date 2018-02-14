d670d63af9c9f7fd784795c4e398b7a5dd1507cf
Sergey I. Mokhov <si.mokhov@gmail.com>
2018-02-14 13:02:20 +0300

    Add JavaDoc to retrieveFile method

--- src/main/java/com/sstd/utils/ftp/SstdFtpManager.java
+++ src/main/java/com/sstd/utils/ftp/SstdFtpManager.java
@@ -20,6 +20,16 @@ public class SstdFtpManager {
         return sstdFtpConnection.getFtpClient();
     }
 
+    /**
+     * @param ftpFile   FTPFile
+     * @param localPath String
+     *                  Path to store local file
+     *                  Can Throw FileNotFoundException
+     * @return boolean
+     * @throws IOException Throws if local FileNotFoundException or
+     *                     an I/O error occurs while either sending a
+     *                     command to the server or receiving a reply from the server.
+     */
     private boolean retrieveFile(FTPFile ftpFile, String localPath) throws IOException {
         OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(localPath));
         return getClient().retrieveFile(ftpFile.getLink(), outputStream);
