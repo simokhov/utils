2c8fc191a6c98d8ada3c9ee6e8ff9147eaffdc6d
Sergey I. Mokhov <si.mokhov@gmail.com>
2018-02-14 13:13:04 +0300

    Add getRemoteFile method

--- src/main/java/com/sstd/utils/ftp/SstdFtpManager.java
+++ src/main/java/com/sstd/utils/ftp/SstdFtpManager.java
@@ -16,9 +16,15 @@ import java.io.OutputStream;
 public class SstdFtpManager {
 
     private SstdFtpConnection sstdFtpConnection;
+    private SstdFtpProcessorInterface sstdFtpProcessor;
 
     public SstdFtpManager(SstdFtpConnection sstdFtpConnection) {
         this.sstdFtpConnection = sstdFtpConnection;
+    }
+
+    public SstdFtpManager(SstdFtpConnection sstdFtpConnection, SstdFtpProcessorInterface sstdFtpProcessor) {
+        this.sstdFtpConnection = sstdFtpConnection;
+        this.sstdFtpProcessor = sstdFtpProcessor;
     }
 
     private FTPClient getClient() {
@@ -38,5 +44,38 @@ public class SstdFtpManager {
     private boolean retrieveFile(FTPFile ftpFile, String localPath) throws IOException {
         OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(localPath));
         return getClient().retrieveFile(ftpFile.getLink(), outputStream);
+    }
+
+    /**
+     * @param ftpFile   FTPFile
+     * @param localPath String
+     *                  Path to store local file
+     *                  Can Throw FileNotFoundException
+     * @return boolean
+     * @throws IOException Throws if local FileNotFoundException or
+     *                     an I/O error occurs while either sending a
+     *                     command to the server or receiving a reply from the server.
+     */
+    public boolean getRemoteFile(FTPFile ftpFile, String localPath) throws IOException {
+
+        boolean result = false;
+
+        if (sstdFtpProcessor != null) {
+
+            //        Before downloading stuff
+            sstdFtpProcessor.beforeRetrieve(ftpFile);
+
+            // Validate file and retrieve from remote host
+            if (sstdFtpProcessor.isValid(ftpFile)) {
+                result = retrieveFile(ftpFile, localPath);
+            }
+
+            //        After downloading stuff
+            sstdFtpProcessor.afterRetrieve(ftpFile);
+        } else {
+            result = retrieveFile(ftpFile, localPath);
+        }
+
+        return result;
     }
 }
